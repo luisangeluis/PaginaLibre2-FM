601a74370541b79b1600bdd729ac83b50989efde
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-05-04 15:59:17 -0500

    indicadores dinamicos
    Se agregan indicadores minuto 51

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,57 @@
 // alert('hola');
 
 const fila = document.querySelector('.carousel');
-const peliculas = document.querySelector('.pelicula');
+const peliculas = document.querySelectorAll('.pelicula');
 
 const btnflechaDerecha = document.querySelector('#flecha-derecha');
+const btnflechaIzquierda = document.querySelector('#flecha-izquierda');
+
+//Variables de paginacion
+
+const numeroPaginas = Math.ceil(peliculas.length/5);
+const divIndicadores = document.querySelector('.indicadores');
 
 
+// Evento flecha derecha carousel
 btnflechaDerecha.addEventListener('click',()=>{
+    //scrollLeft para indicar la posicion del scroll del elemento
     // fila.scrollLeft=1000;
+
+    // offsetWidth returna el ancho del elemento
     // let off = fila.offsetWidth;
 
-    fila.scrollLeft +=fila.offsetWidth/2
-    console.log(fila.offsetWidth);
+    // fila.scrollLeft +=fila.offsetWidth/2
+    // console.log(fila.offsetWidth);
+
+    console.log(fila.scrollLeft);
+    console.log(`ancho del carousel ${fila.offsetWidth}`);
+    fila.scrollLeft=1000;
+
+    fila.scrollLeft +=fila.offsetWidth/2;
+
 })
+// Evento flecha izquierda carousel
+btnflechaIzquierda.addEventListener('click',()=>{
+    fila.scrollLeft-=fila.offsetWidth/2;
+    
+});
+
+console.log(numeroPaginas);
+
+for(let i=0; i<numeroPaginas;i++){
+    const btnIndicador = document.createElement('button');
+    
+    if(i==0){
+        btnIndicador.classList.add('activo');
+    }
+    divIndicadores.appendChild(btnIndicador);
+}
+
+divIndicadores.addEventListener('click',(e)=>{
+    fila.scrollLeft = 
+    console.dir(e);
+});
+
 
 
 
