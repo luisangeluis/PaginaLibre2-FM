6acbc1ce5e839f9b92268f4c2ef16a2b031909fc
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-05-13 14:32:11 -0500

    La parte del carousel está terminado.

--- JS/main.js
+++ JS/main.js
@@ -8,12 +8,12 @@ const btnflechaIzquierda = document.querySelector('#flecha-izquierda');
 
 //Variables de paginacion
 
-const numeroPaginas = Math.ceil(peliculas.length/5);
+const numeroPaginas = Math.ceil(peliculas.length / 5);
 const divIndicadores = document.querySelector('.indicadores');
 
 
 // Evento flecha derecha carousel
-btnflechaDerecha.addEventListener('click',()=>{
+btnflechaDerecha.addEventListener('click', () => {
     //scrollLeft para indicar la posicion del scroll del elemento
     // fila.scrollLeft=1000;
 
@@ -25,60 +25,86 @@ btnflechaDerecha.addEventListener('click',()=>{
 
     console.log(fila.scrollLeft);
     console.log(`ancho del carousel ${fila.offsetWidth}`);
-    fila.scrollLeft=1000;
+    fila.scrollLeft = 1000;
 
-    fila.scrollLeft +=fila.offsetWidth;
+    fila.scrollLeft += fila.offsetWidth;
 
     const indicadorActivo = document.querySelector('.indicadores .activo');
-    if(indicadorActivo.nextSibling){
+    if (indicadorActivo.nextSibling) {
         indicadorActivo.nextSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
     }
 
 })
 // Evento flecha izquierda carousel
-btnflechaIzquierda.addEventListener('click',()=>{
-    fila.scrollLeft-=fila.offsetWidth;
+btnflechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
 
     const indicadorActivo = document.querySelector('.indicadores .activo');
-    if(indicadorActivo.previousSibling){
+    if (indicadorActivo.previousSibling) {
         indicadorActivo.previousSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
     }
-    
+
 });
 //Eventos de paginacion
 console.log(numeroPaginas);
 
-for(let i=0; i<numeroPaginas;i++){
+for (let i = 0; i < numeroPaginas; i++) {
 
     const btnIndicador = document.createElement('button');
-    
-    if(i==0){
+
+    if (i == 0) {
         btnIndicador.classList.add('activo');
     }
 
     divIndicadores.appendChild(btnIndicador);
 
-    divIndicadores.children[i].addEventListener('click',(e)=>{
-        fila.scrollLeft = i*fila.offsetWidth;
+    divIndicadores.children[i].addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
 
         // if(e.target.classList.contains('activo')){
         //     console.log('hola');
         // }
         document.querySelector('.indicadores .activo').classList.remove('activo');
-        e.target.classList.add('activo');        
-        
+        e.target.classList.add('activo');
+
     });
 
 }
 
 //Efecto hover de las peliculas
 
-for(let x = 0;)
-document.querySelector('.peliculas-group').children[].addEventListener('click',()=>{
+// const peliculasGroup = document.querySelector('.peliculas-group');
 
-});
+
+for (let x = 0; x < peliculas.length; x++) {
+
+
+    peliculas[x].addEventListener('mouseenter', (e) => {
+        let peli = e.currentTarget;
+        console.log(peli);
+
+        setTimeout(() => {
+
+            peliculas.forEach(p => {
+                p.classList.remove('hover');
+            })
+
+            peli.classList.add('hover');
+        },300)
+    })
+}
+
+fila.addEventListener('mouseleave',()=>{
+    peliculas.forEach(p => {
+        p.classList.remove('hover');
+    })
+})
+
+// peliculas.addEventListener('click',()=>{
+
+
 
 
 
